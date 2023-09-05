2151ce31e1db8f99a15c76688c0aa9b49e7dc2f2
Huoyly <horthuoyly.cubc@gmail.com>
2023-09-05 11:09:15 +0700

    test commit

--- app/src/main/java/com/example/apptravel/ui/activity/StarterActivity.kt
+++ app/src/main/java/com/example/apptravel/ui/activity/StarterActivity.kt
@@ -2,18 +2,48 @@ package com.example.apptravel.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.apptravel.R
 import com.example.apptravel.attractionPlace.ui.activity.HomeActivity
-
+@Suppress("DEPRECATION")
 class StarterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_starter)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+
         startActivity(Intent(this, HomeActivity::class.java))
         finish()
+    }
+}
+@Suppress("DEPRECATION")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_starter)
+
+        // This is used to hide the status bar and make
+        // the splash screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
