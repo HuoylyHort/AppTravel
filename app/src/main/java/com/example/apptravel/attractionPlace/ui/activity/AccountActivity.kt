a9bad5c4fcec32e69b97e4c38e04ad3a3089cfa2
Huoyly <horthuoyly.cubc@gmail.com>
2023-09-07 15:04:20 +0700

    [View] Implement  Account Activity

--- app/src/main/java/com/example/apptravel/attractionPlace/ui/activity/AccountActivity.kt
+++ app/src/main/java/com/example/apptravel/attractionPlace/ui/activity/AccountActivity.kt
@@ -1,4 +1,12 @@
 package com.example.apptravel.attractionPlace.ui.activity
 
-class AccountActivity {
+import androidx.fragment.app.Fragment
+import com.example.apptravel.attractionPlace.ui.fragment.AccountFragment
+import com.example.apptravel.databinding.ActivityAccountBinding
+import com.example.apptravel.ui.activity.BaseActivity
+
+class AccountActivity : BaseActivity<ActivityAccountBinding> (){
+    override fun inflateLayout(): ActivityAccountBinding = ActivityAccountBinding.inflate(layoutInflater)
+
+    override fun getDefaultFragment(): Fragment? = AccountFragment.newInstance()
 }
